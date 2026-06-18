cf7090851ca3d4fb40420abcb54470d46e9445ae
Shivaprakash D <shivaprakashd@gmail.com>
2026-06-18 13:25:41 +0530

    Day 4 - Added CRM menu and add customer

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import json
 
 from customer import Customer
 
+def display_menu():
+
+    print("\nCRM Lite")
+    print("========")
+    print("1. Display Customers")
+    print("2. Add Customer")
+    print("3. Exit")
 
 def load_customers():
 
@@ -16,7 +23,7 @@ def load_customers():
         customer = Customer.from_dict(customer_data)
         customers.append(customer)
 
-        return customers
+    return customers
 
 def display_customers(customers):
 
@@ -26,9 +33,46 @@ def display_customers(customers):
     for customer in customers:
         customer.display()
 
+def add_customer(customers):
+
+    customer_id = int(input("Enter Customer ID: "))
+    name = input("Enter Name: ")
+    city = input("Enter City: ")
+
+    customer = Customer(
+        customer_id,
+        name,
+        city
+    )
+
+    customers.append(customer)
+
+    print("Customer added")
+
 customers = load_customers()
 
-display_customers(customers)
+while True:
+
+    display_menu()
+
+    choice = input("\nEnter choice: ")
+
+    if choice == "1":
+
+        display_customers(customers)
+
+    elif choice == "2":
+
+        add_customer(customers)
+
+    elif choice == "3":
+
+        print("Goodbye")
+        break
+
+    else:
+
+        print("Invalid choice")
 
 
 
